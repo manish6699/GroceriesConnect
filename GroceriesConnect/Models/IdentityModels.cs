b4e3f155738314f15097194cdce87ff48eba194f
manish#xman <manishreddy.v6699@gmail.com>
2021-03-01 00:27:51 +0530

    Add one to many relation between vendor (AspNetUser or ApplicationUser) and catalog class

--- GroceriesConnect/Models/IdentityModels.cs
+++ GroceriesConnect/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using GroceriesConnect.Models.GroceriesConnectModels;
@@ -10,6 +11,8 @@ namespace GroceriesConnect.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public List<Catalog> Catalogs { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
