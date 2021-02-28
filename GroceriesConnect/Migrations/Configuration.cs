90d82b6eaafa4e13e46cb44821250fa3aa0ab795
manish#xman <manishreddy.v6699@gmail.com>
2021-02-28 12:01:06 +0530

    Add Class Category

---
+++ GroceriesConnect/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace GroceriesConnect.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GroceriesConnect.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(GroceriesConnect.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ GroceriesConnect/Models/GroceriesConnectModels/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GroceriesConnect.Models.GroceriesConnectModels
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please select valid category")]
+        [Display(Name = "Category Name")]
+        [MaxLength(128)]
+        public string CategoryName { get; set; }
+    }
+}
